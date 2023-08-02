a5c36460e0feb792670bb2c196a9e8d256172198
Glenn Hope <glenn.alexander.hope@gmail.com>
2023-08-02 14:43:06 -0700

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
+if [ "$EUID" -ne 0 ]; then 
+    echo "Please run as root!"
+    exit
 fi
 
 cc beep.c
@@ -11,5 +11,5 @@ cp my_beep /usr/local/bin/my_beep
 
 pip3 install flask
 
-echo 'Now run `visudo` and add the following line to the end of the file:'
-echo '%users  ALL=/usr/local/bin/my_beep'
+echo '# Now run `visudo` and add the following line to the end of the file:'
+echo '%wheel ALL=(ALL:ALL) NOPASSWD: /usr/local/bin/my_beep'
